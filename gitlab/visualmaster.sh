d9e77d0e1fb081bd25845370d58a0f2202ccb83f
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2020-10-22 20:47:20 +0200

    Different checkout

--- gitlab/visualmaster.sh
+++ gitlab/visualmaster.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-git branch -l
-git branch -a
+export PS4='(${BASH_SOURCE}:${LINENO}): - [$?] $ '
+
+printenv
 git status
 git clone --depth=1 https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/mvasseur/domjudeg.git t -b master
 #git checkout master
