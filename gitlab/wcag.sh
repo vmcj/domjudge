ad7c330baf171430173ebd697c90d1d60ed6eb8c
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2020-10-31 00:09:28 +0100

    Try with all standards

--- gitlab/wcag.sh
+++ gitlab/wcag.sh
@@ -104,9 +104,15 @@ do
 	do
 		section_start ${file//\//} $file
 		# T is reasonable amount of errors to allow to not break
-		su domjudge -c "pa11y -T $ACCEPTEDERR -E '#menuDefault > a > button' --reporter json ./$file" | python -m json.tool
-	        ERR=`su domjudge -c "pa11y -T $ACCEPTEDERR -E '#menuDefault > a > button' --reporter csv ./$file" | wc -l`
+		su domjudge -c "pa11y --runner axe -T $ACCEPTEDERR -E '#menuDefault > a > button' --reporter json ./$file" | python -m json.tool
+	        ERR=`su domjudge -c "pa11y --runner axe -T $ACCEPTEDERR -E '#menuDefault > a > button' --reporter csv ./$file" | wc -l`
 		FOUNDERR=$((ERR+FOUNDERR-1)) # Remove header row
+		for standard in Section508 WCAG2A WCAG2AA WCAG2AAA
+		do
+			su domjudge -c "pa11y -s $standard -T $ACCEPTEDERR -E '#menuDefault > a > button' --reporter json ./$file" | python -m json.tool
+	        	ERR=`su domjudge -c "pa11y -s $standard -T $ACCEPTEDERR -E '#menuDefault > a > button' --reporter csv ./$file" | wc -l`
+			FOUNDERR=$((ERR+FOUNDERR-1)) # Remove header row
+		done
 		section_end $file
 	done
 done
