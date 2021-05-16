2941597d93650e1227a4483776056194a4057ce1
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-05-16 20:47:29 +0200

    stash

--- webapp/tests/Unit/Controller/Jury/UserControllerTest.php
+++ webapp/tests/Unit/Controller/Jury/UserControllerTest.php
@@ -6,11 +6,12 @@ use App\Entity\User;
 
 class UserControllerTest extends JuryControllerTest
 {
-    protected static $baseUrl        = '/jury/users';
-    protected static $exampleEntries = ['admin','judgehost','Administrator','team'];
-    protected static $shortTag       = 'user';
-    protected static $deleteEntities = ['username' => ['demo']];
-    protected static $getIDFunc      = 'getUserid';
-    protected static $className      = User::class;
-    protected static $DOM_elements   = ['h1' => ['Users']];
+    protected static $baseUrl         = '/jury/users';
+    protected static $exampleEntries  = ['admin','judgehost','Administrator','team'];
+    protected static $shortTag        = 'user';
+    protected static $deleteEntities  = ['username' => ['demo']];
+    protected static $getIDFunc       = 'getUserid';
+    protected static $className       = User::class;
+    protected static $DOM_elements    = ['h1' => ['Users']];
+    protected static $delete_internal = ['username' => ['judgehost','demo']];
 }
