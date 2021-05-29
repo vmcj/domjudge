b5627a3296d4e1e8dc80bc0fe299231874f637de
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-05-29 12:17:07 +0200

    Add fixture and start with testing solution
    Small fix
    
    Reeanble small pieces

--- webapp/src/DataFixtures/Test/SampleSubmissionsFixture.php
+++ webapp/src/DataFixtures/Test/SampleSubmissionsFixture.php
@@ -4,7 +4,11 @@ namespace App\DataFixtures\Test;
 
 use App\Entity\Contest;
 use App\Entity\ContestProblem;
+use App\Entity\Judgehost;
+use App\Entity\JudgeTask;
 use App\Entity\Judging;
+use App\Entity\JudgingRun;
+use App\Entity\JudgingRunOutput;
 use App\Entity\Language;
 use App\Entity\Submission;
 use App\Entity\Team;
@@ -19,10 +23,19 @@ class SampleSubmissionsFixture extends AbstractTestDataFixture
             // team name,         problem shortname, language, submittime,            entry point, result
             ['DOMjudge',          'A',               'cpp',    '2021-01-01 12:34:56', null,        'success'],
             ['Example teamname',  'C',               'java',   '2021-03-04 12:00:00', 'Main',      'wrong-answer'],
+            //['DOMjudge',          'hello',           'cpp',    '2021-01-01 12:34:56', null,        'correct'],
+            ['DOMjudge',          'hello',           'cpp',    '2021-01-01 12:34:56', null,        'success'],
+            ['Example teamname',  'boolfind',        'java',   '2021-03-04 12:00:00', 'Main',      'wrong-answer'],
+            //['Example teamname',  'fltcmp',          'java',   '2021-03-05 11:09:45', 'Main',      'wrong-answer'],
+            //['Example teamname',  'fltcmp',          'java',   '2021-03-05 11:12:05', 'Main',      'wrong-answer'],
         ];
 
         /** @var Contest $contest */
         $contest = $manager->getRepository(Contest::class)->findOneBy(['shortname' => 'demo']);
+        /*$judgehost = (new Judgehost)
+            ->setHostname('fixture-judgehost');
+        $manager->persist($judgehost);
+        $manager->flush();*/
         foreach ($submissionData as $index => $submissionItem) {
             $problem = $contest->getProblems()->filter(
                 fn(ContestProblem $problem) => $problem->getShortname() === $submissionItem[1]
@@ -34,15 +47,59 @@ class SampleSubmissionsFixture extends AbstractTestDataFixture
                 ->setLanguage($manager->getRepository(Language::class)->find($submissionItem[2]))
                 ->setSubmittime(Utils::toEpochFloat($submissionItem[3]))
                 ->setEntryPoint($submissionItem[4]);
+            //$manager->persist($submission);
+            //$manager->flush();
+            // Partially add the judging on the testcase, the judging_run_output is not filled.
             $judging = (new Judging())
                 ->setContest($contest)
+                //->setSubmission($submission)
                 ->setStarttime(Utils::toEpochFloat($submissionItem[3]))
                 ->setEndtime(Utils::toEpochFloat($submissionItem[3]) + 5)
+                //->setEndtime(Utils::toEpochFloat($submissionItem[3]) + count($testCases))
+                //->setJudgehost($judgehost)
+                //->setValid(true)
                 ->setResult($submissionItem[5]);
             $submission->addJudging($judging);
             $manager->persist($submission);
             $manager->persist($judging);
             $manager->flush();
+            /*foreach($testCases as $testCase) {
+                $rank = $testCase->getRank();
+                $judgeTask = (new JudgeTask())
+                    ->setType('judging_run')
+                    ->setTestcaseid($testCase->getTestcaseid())
+                    ->setPriority(1)
+                    ->setJobid($judging->getJudgingid())
+                    ->setSubmitid($submission->getSubmitid());
+                if ($submissionItem[5] === 'correct' || $rank === 1) {
+                    // Judgetasks are not started when a non-correct result is found
+                    $judgeTask = $judgeTask
+                        ->setJudgehost($judgehost)
+                        ->setValid(true)
+                        ->setStarttime(Utils::toEpochFloat($submissionItem[3]) + $rank);
+                } else {
+                    $judgeTask = $judgeTask
+                        ->setValid(false);
+                }
+                $manager->persist($judgeTask);
+                $judgingRun = (new JudgingRun())
+                    ->setJudging($judging)
+                    ->setJudgeTask($judgeTask)
+                    ->setTestcase($testCase);
+                if ($submissionItem[5] === 'success' || $rank === 1) {
+                    $judgingRun = $judgingRun
+                        ->setRuntime(1)
+                        ->setRunresult($submissionItem[5])
+                        ->setEndtime(Utils::toEpochFloat($submissionItem[3]) + 1 + $testCase->getRank());
+                    $judgingrun_out = (new JudgingRunOutput)
+                        ->setRun($judgingRun)
+                        ->setOutputRun("OUTPUT")
+                        ->setOutputDiff("DIFF");
+                    $manager->persist($judgingrun_out);
+                }
+                $manager->persist($judgingRun);
+            }
+            $manager->flush();*/
             // Add a reference, since the submission ID changes during testing because of the auto increment
             $this->addReference(sprintf('%s:%d', static::class, $index), $submission);
         }
--- webapp/tests/Unit/Controller/Jury/SubmissionControllerTest.php
+++ webapp/tests/Unit/Controller/Jury/SubmissionControllerTest.php
@@ -38,4 +38,20 @@ class SubmissionControllerTest extends BaseTestCase
             }
         }
     }
+
+    /*
+     * When lazy evaluation is enabled testcases are evaluated until a non-correct
+     * result is found. This tests that enabling further evaluation does not trigger
+     * the evaluation for other submissions
+     * @dataProvider provideSubmissions
+     */
+    //public function JudgeRemaining(): void
+    //{
+    //    // Load wrong submissions
+    //    #$this->loadFixture("SampleSubmissionsFixture");
+    //    self::assertEquals('yes','yes');
+    //    // Check that for the first 2 the button is not available
+    //    // Check that for the 2nd 2 the button is there
+    //    // Check that when pressing the 1st button the 2nd is still available 
+    //}
 }
