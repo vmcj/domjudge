1de43f96b757d765ff6d334ff95e6d31870edef2
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-07-18 13:38:40 +0200

    Skip the executables for now

--- webapp/tests/Unit/Controller/Jury/ExecutableControllerTest.php
+++ webapp/tests/Unit/Controller/Jury/ExecutableControllerTest.php
@@ -13,4 +13,12 @@ class ExecutableControllerTest extends JuryControllerTest
     protected static $getIDFunc        = 'getExecid';
     protected static $className        = Executable::class;
     protected static $DOM_elements     = ['h1' => ['Executables']];
+    protected static $addForm          = 'executable_upload[';
+    protected static $addEntitiesShown = ['shortname','name'];
+    protected static $addEntities      = [];/*['type' => 'compare',
+                                           'archives' => ''],
+                                          ['type' => 'compile',
+                                           'archives' => ''],
+                                          ['type' => 'run',
+                                           'archives' => '']];*/
 }
