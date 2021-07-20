a58c6d69f8d1713f8e6d4c3f0d40d7f8d49d971b
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-07-20 16:13:22 +0200

    Extend unit add tests
    We now also test for boolean values and enumarations. When lists are
    summarized as counts we can indicate this with another field.

--- webapp/tests/Unit/Controller/Jury/LanguagesControllerTest.php
+++ webapp/tests/Unit/Controller/Jury/LanguagesControllerTest.php
@@ -13,7 +13,43 @@ class LanguagesControllerTest extends JuryControllerTest
     protected static $getIDFunc        = 'getLangid';
     protected static $className        = Language::class;
     protected static $DOM_elements     = ['h1' => ['Languages']];
+    protected static $addPlus          = 'extensions';
     protected static $addForm          = 'language[';
     protected static $addEntitiesShown = ['langid','externalid','name','timefactor'];
-    protected static $addEntities      = [];
+    protected static $addEntities      = [/*['langid' => 'simple',
+    'externalid' => 'simple',
+    'name' => 'Simple',
+    'requireEntryPoint' => '0',
+    'entryPointDescription' => '',
+    'allowSubmit' => '1',
+    'allowJudge' => '1',
+    'timeFactor' => '1',
+    'compileExecutable' => 'java_javac',
+    'extensions' => ['0' => 'extension'],
+    'filterCompilerFiles' => '1'],
+   ['langid' => 'ext',
+    'externalid' => 'diffext',
+    'name' => 'External'],*/
+   /*['langid' => 'entry',
+    'requireEntryPoint' => '1',
+    'entryPointDescription' => 'shell'],
+   ['langid' => 'nosub',
+    'allowSubmit' => '0'],
+   ['langid' => 'nojud',
+    'allowJudge' => '0'],
+   ['langid' => 'timef1',
+    'timeFactor' => '3'],
+   ['langid' => 'timef2',
+    'timeFactor' => '1.3'],
+   ['langid' => 'timef3',
+    'timeFactor' => '0.5'],
+   ['langid' => 'comp',
+    'compileExecutable' => 'java_javac'],
+   ['langid' => 'multex',
+    'extensions' => ['0' => 'a',
+                     '1' => 'b',
+                     '2' => '1',
+                     '3' => ',']],
+   ['langid' => 'nofilt',
+'filterCompilerFiles' => '0']*/];
 }
