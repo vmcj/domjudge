f99c3525d9d6e3bdf55e9349da94f69952c540ba
MCJ Vasseur <14887731+mvr320@users.noreply.github.com>
2021-09-09 23:30:30 +0200

    Function needed at both locations

--- webapp/src/Controller/Jury/UserController.php
+++ webapp/src/Controller/Jury/UserController.php
@@ -223,6 +223,17 @@ class UserController extends BaseController
         ]);
     }
 
+    public function checkPasswordLength(User $user, $form) {
+        if ($user->getPlainPassword() && strlen($user->getPlainPassword())<$this->minPasswordLength) {
+            $this->addFlash('danger', "Password should be " . $this->minPasswordLength . "+ chars.");
+            return $this->render('jury/user_edit.html.twig', [
+                'user' => $user,
+                'form' => $form->createView(),
+                'min_password_length' => $this->minPasswordLength,
+            ]);
+        }
+    }
+
     /**
      * @Route("/{userId<\d+>}/edit", name="jury_user_edit")
      * @IsGranted("ROLE_ADMIN")
@@ -320,6 +331,9 @@ class UserController extends BaseController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            if ($errorResult = $this->checkPasswordLength($user, $form)) {
+                return $errorResult;
+            }
             $this->em->persist($user);
             $this->saveEntity($this->em, $this->eventLogService, $this->dj, $user, null, true);
             return $this->redirect($this->generateUrl(
