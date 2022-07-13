26b2391ba81381c2e7d6366e61a97515f74d6545
MCJ Vasseur <14887731+vmcj@users.noreply.github.com>
2022-07-13 08:39:17 +0200

    More deleteList endpoints

--- webapp/src/Controller/Jury/TeamCategoryController.php
+++ webapp/src/Controller/Jury/TeamCategoryController.php
@@ -131,6 +131,36 @@ class TeamCategoryController extends BaseController
     }
 
     /**
+     * @Route("/deleteList", name="jury_team_categories_delete")
+     * @IsGranted("ROLE_ADMIN")
+     */
+    public function deleteListAction(Request $request): Response
+    {
+        $checkboxPrefix = 'ident';
+        $entitiesToDelete = [];
+        foreach (array_keys($request->request->all()) as $key) {
+            if (strpos($key, $checkboxPrefix) !== 0) {
+                continue;
+            }
+            /** @var Contest $contest */
+            $entityId = substr($key, strlen($checkboxPrefix));
+            $entity = $this->em->getRepository(Contest::class)->find($entityId);
+            if (!$entity) {
+                throw new NotFoundHttpException(sprintf('Team category with ID %s not found', $entityId));
+            }
+            $entitiesToDelete[] = $entity;
+        }
+
+        if (count($entitiesToDelete)===0) {
+            $this->addFlash('warning', 'No team categories selected.'); 
+            return $this->redirectToRoute('jury_team_categories');
+        }
+
+        return $this->deleteEntities($request, $this->em, $this->dj, $this->eventLogService, $this->kernel,
+                                     $entitiesToDelete, $this->generateUrl('jury_team_categories'));
+    }
+
+    /**
      * @Route("/{categoryId<\d+>}", name="jury_team_category")
      * @throws NoResultException
      * @throws NonUniqueResultException
