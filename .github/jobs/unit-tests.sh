a3563051a3953048ed27702ce54da58a92919e3a
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2023-02-26 22:23:45 +0100

    Move unit tests to GHA
    Not all uses are now implemented, the deprications is still missing for
    example.
    
    Renamed the current job which checked if our codecov was out of date to
    fix tab completion.

---
+++ .github/jobs/unit-tests.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+. .github/jobs/data/gha_ci_bashrc
+
+section_start chown_checkout
+git config --global --add safe.directory /__w/domjudge/domjudge
+section_end chown_checkout
+
+section_start current_database_domjudge "Currently installed databases (domjudge)"
+set +eu
+echo "show databases" | mysql -hsqlserveranother -udomjudge -pdomjudge
+echo "show databases" | mysql -hsqlserver -udomjudge -pdomjudge
+set -eu
+section_end current_database_domjudge
+
+section_start current_database_root "Currently installed databases (root)"
+set +eu
+echo "show databases" | mysql -hsqlserver -uroot -pdomjudge
+set -eu
+section_end current_database_root
+
+export version=$1
+unittest=$2
+[ "$version" = "8.1" ] && CODECOVERAGE=1 || CODECOVERAGE=0
+
+show_phpinfo $version
+
+.github/jobs/base.sh
+
+# Add team to admin user
+echo "INSERT INTO userrole (userid, roleid) VALUES (1, 3);" | mysql domjudge
+echo "UPDATE user SET teamid = 1 WHERE userid = 1;" | mysql domjudge
+
+# Copy the .env.test file, as this is normally not done during
+# installation and we need it.
+cp webapp/.env.test /opt/domjudge/domserver/webapp/
+
+# We also need the composer.json for PHPunit to detect the correct directory.
+cp composer.json /opt/domjudge/domserver/
+
+cd /opt/domjudge/domserver
+
+export APP_ENV="test"
+
+# Run phpunit tests.
+pcov=""
+phpcov=""
+if [ "$CODECOVERAGE" -eq 1 ]; then
+    phpcov="-dpcov.enabled=1 -dpcov.directory=webapp/src"
+    pcov="--coverage-html=${PWD}/coverage-html --coverage-clover coverage.xml"
+fi
+set +e
+php $phpcov lib/vendor/bin/phpunit -c webapp/phpunit.xml.dist webapp/tests/$unittest --log-junit ${DIR}/unit-tests.xml --colors=never $pcov > "$ARTIFACTS"/phpunit.out
+UNITSUCCESS=$?
+set -e
+CNT=0
+if [ $CODECOVERAGE -eq 1 ]; then
+    CNT=$(sed -n '/Generating code coverage report/,$p' "$ARTIFACTS"/phpunit.out | grep -v DoctrineTestBundle | grep -cv ^$)
+    FILE=deprecation.txt
+    sed -n '/Generating code coverage report/,$p' "$ARTIFACTS"/phpunit.out > "$DIR/$FILE"
+    if [ $CNT -le 12 ]; then
+        STATE=success
+    else
+        STATE=failure
+    fi
+    ORIGINAL="gitlab.com/DOMjudge"
+    REPLACETO="domjudge.gitlab.io/-"
+    # Copied from CCS
+    #curl https://api.github.com/repos/domjudge/domjudge/statuses/$CI_COMMIT_SHA \
+    #  -X POST \
+    #  -H "Authorization: token $GH_BOT_TOKEN_OBSCURED" \
+    #  -H "Accept: application/vnd.github.v3+json" \
+    #  -d "{\"state\": \"$STATE\", \"target_url\": \"${CI_JOB_URL/$ORIGINAL/$REPLACETO}/artifacts/$FILE\", \"description\":\"Symfony deprecations\", \"context\": \"Symfony deprecation\"}"
+fi
+if [ $UNITSUCCESS -ne 0 ]; then
+    exit $UNITSUCCESS
+fi
+
+if [ $CODECOVERAGE -eq 1 ]; then
+    section_start uploadcoverage "Upload code coverage"
+    # Only upload when we got working unit-tests.
+    set +u # Uses some variables which are not set
+    # shellcheck disable=SC1090
+    . $DIR/.github/jobs/uploadcodecov.sh &>/dev/zero
+    set -u # Undo set dance
+    section_end uploadcoverage
+fi
