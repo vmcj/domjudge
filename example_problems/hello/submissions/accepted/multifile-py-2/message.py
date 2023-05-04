7795c1d2a0d2b9b4d99ce303e031054789f4557e
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2023-05-04 09:18:16 +0200

    Add extra python example submissions to test the same as for Java/C++

--- example_problems/hello/submissions/accepted/multifile-py-2/message.py
+++ example_problems/hello/submissions/accepted/multifile-py-2/message.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 '''
 This is an auxiliary source file that declares another public class 'message'.
 '''
@@ -10,3 +11,17 @@ class Message:
 
     def myPrint(self):
         print(self.msg)
+=======
+'''
+This is an auxiliary source file that declares another public class 'message'.
+'''
+
+class Message:
+    msg = ""
+
+    def __init__(self):
+        self.msg = "Hello world!"
+
+    def myPrint(self):
+        print(self.msg)
+>>>>>>> 59b8fc496 (Add extra python example submissions to test the same as for Java/C++)
