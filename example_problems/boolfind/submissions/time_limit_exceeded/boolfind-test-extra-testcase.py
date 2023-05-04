33c3506fcd90a6cdae050ee53e6eb46c62e971bd
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2023-05-04 09:18:16 +0200

    Add python examples to have the same tests as for Java/C++

--- example_problems/boolfind/submissions/time_limit_exceeded/boolfind-test-extra-testcase.py
+++ example_problems/boolfind/submissions/time_limit_exceeded/boolfind-test-extra-testcase.py
@@ -2,10 +2,17 @@
 Sample solution in Python for the "boolfind" interactive problem.
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 This waits for one more testcase which is not specified.
 
 =======
 >>>>>>> e281f8da1 (Add python examples to have the same tests as for Java/C++)
+=======
+=======
+This waits for one more testcase which is not specified.
+
+>>>>>>> 1ad831fdc (Add python examples to have the same tests as for Java/C++)
+>>>>>>> 4f34a95ba (Add python examples to have the same tests as for Java/C++)
 @EXPECTED_RESULTS@: TIMELIMIT
 '''
 
