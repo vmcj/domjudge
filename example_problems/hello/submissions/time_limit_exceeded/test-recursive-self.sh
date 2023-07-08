38bf72f9bc0c7e902eac0818c3c7c65d788651b2
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2023-07-08 10:58:36 +0200

    Cleanup

--- example_problems/hello/submissions/time_limit_exceeded/test-recursive-self.sh
+++ example_problems/hello/submissions/time_limit_exceeded/test-recursive-self.sh
@@ -3,7 +3,7 @@
 # See: https://codegolf.stackexchange.com/a/24488
 # This keeps on calling itself so runs forever.
 
-shell_double_arguments() { 
+shell_double_arguments() {
     shell_double_arguments $@$@
 }
 shell_double_arguments "single"
