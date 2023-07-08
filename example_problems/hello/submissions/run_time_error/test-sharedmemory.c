c67faa17a541077bffa61b19762d7ee869376823
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2023-07-08 13:52:40 +0200

    cleanup

--- example_problems/hello/submissions/run_time_error/test-sharedmemory.c
+++ example_problems/hello/submissions/run_time_error/test-sharedmemory.c
@@ -1,18 +1,20 @@
-#include <sys/types.h>
-#include <sys/shm.h>
 #include <string.h>
 
-#define SHMSZ (2*1024*1024) /*Ubuntu rejects shared allocations larger than about 2MiB*/
+#include <sys/types.h>
+#include <sys/shm.h>
 
-main() {
+#define SHMSZ (3*1024*1024) /*Ubuntu rejects shared allocations larger than about 2MiB*/
+
+int main() {
     int shmid;
     key_t key = 0xF111; /* Lets use `Fill' as our first ID.*/
     char *shm;
 
     while(1) { /* Like malloc, but using shared memory */
         if ((shmid = shmget(key, SHMSZ, IPC_CREAT|0666)) < 0){return 1;}/*Get shared memory*/
-        if ((shm = shmat(shmid, NULL, 0)) == (void *) -1) { return 1; } /*Attach it        */
+        if ((shm = shmat(shmid, NULL, 0)) == (void *) -1) { return 2; } /*Attach it        */
         memset(shm,0,SHMSZ);                                            /*Fill it up       */
         key++;                                                          /*On to the next ID*/
     }
+    return 0;
 }
