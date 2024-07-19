e40e991cbba3e29ba8e1de680f29351888ee2093
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2024-07-19 22:31:13 +0200

    Check which rules are not needed

--- .github/jobs/webstandard.sh
+++ .github/jobs/webstandard.sh
@@ -151,6 +151,8 @@ else
         # Ignore the menu items, we make those inactive by design
         FLTRMENUITEM="'#menuDefault > ul > li > a'"
         # Ignore the contrast as it is not always properly detected
+        FLTRMENUITEM="#notanelement"
+        FLTRFAS="#notaclass"
         IGNORERULES="--ignore WCAG2AAA.Principle1.Guideline1_4.1_4_6.G18.Fail --ignore WCAG2AAA.Principle1.Guideline1_4.1_4_6.G17.Fail"
         FLTR="$IGNORERULES -E $FLTRPROBBADGE,$FLTRTEAMCAT,$FLTRFAS,$FLTRMENUITEM"
     fi
