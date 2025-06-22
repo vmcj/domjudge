251f48bba5a4e1ae4a93a1c90d4aac1fdfdf0e8c
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2025-06-22 12:06:30 +0200

    Add example for submissions without args
    Encountered during a contest.
    
    The java would not start with such a class. It does compile so this is
    mostly added to make sure that our `detect_main` correctly detects this
    case (as it currently does).

---
+++ example_problems/hello/submissions/compiler_error/main_args.java
@@ -0,0 +1,14 @@
+/*
+ * This submission doesn't read the arguments so is not found
+ * as a valid entrypoint
+ *
+ * @EXPECTED_RESULTS@: COMPILER-ERROR
+ */
+
+import java.io.*;
+
+class Main {
+    public static void main() throws Exception {
+		System.out.print("Hello world!\n");
+    }
+}
