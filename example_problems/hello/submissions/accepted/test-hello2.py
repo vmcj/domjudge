600f87257ef152b2e692ad25239863c7acff8a87
Michael Vasseur <14887731+vmcj@users.noreply.github.com>
2025-06-22 12:27:42 +0200

    Add edge case for carriage return

---
+++ example_problems/hello/submissions/accepted/test-hello2.py
@@ -0,0 +1,7 @@
+# This overwrites the output in the end with the right value.
+# It should trip string comparisons starting from the beginning.
+# 
+#
+# @EXPECTED_RESULTS@: CORRECT,WRONG-ANSWER
+
+print("hallo wereld\rHello world!")
